cc971261b8fad9a78f71a816c117c84437c32eaf
albertmanuel <albertmanuels10@gmail.com>
2025-06-07 16:36:01 +0700

    add test for topping option

--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -2,7 +2,7 @@ import { describe, expect, test } from "vitest";
 import Options from "../Options";
 import { render, screen } from "@testing-library/react";
 
-describe('ScoopOption', () => { 
+describe('Options', () => { 
   test("display image for each scoop option from server", async () => {
     render(<Options optionType="scoops" />)
     const scoopImages = await screen.findAllByRole("img", {name: /scoop$/i})
@@ -11,5 +11,15 @@ describe('ScoopOption', () => {
 
     const altText = scoopImages.map(image => image.alt)
     expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop'])
+  })
+
+  test("display image for each topping option from server", async () => {
+   render(<Options optionType="toppings" />)
+   const toppingImages  = await screen.findAllByRole("img", {name: /topping$/i})
+
+   expect(toppingImages).toHaveLength(3)
+
+   const altText = toppingImages.map(image => image.alt)
+   expect(altText).toEqual(['Cherries topping', 'M&Ms topping', 'Hot fudge topping'])
   })
  })
